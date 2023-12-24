cb31ed7e6a738d98627f789555e29f7d99be899b
Andrey <andryuxa16@yandex.ru>
2023-12-24 21:25:10 +0300

    add commit add Task02.java

---
+++ src/Employee.java
@@ -0,0 +1,35 @@
+public class Employee {
+    private String name;
+    private String type;
+    private double salary;
+
+    public Employee(String name, String type, double salary) {
+        this.name = name;
+        this.type = type;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
---
+++ src/PayrollReport.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PayrollReport {
+
+    public static void main(String[] args) {
+        List employees = new ArrayList();
+        employees.add(new Employee("Иванов", "Ставка", 3000));
+        employees.add(new Employee("Сидоров", "Почасовая", 800));
+        employees.add(new Employee("Петров", "Сдельная", 5500));
+        System.out.println("ФИО\tНалог, %\tСумма (грн)\tК оплате (грн)");
+
+        for (Employee employee : employees) {
+        }
+        double tax = 0.0;
+        Employee employee;
+        if (employee.getType().equals("Ставка") || employee.getType().equals("Почасовая")) {
+            tax = 0.2;
+        } else if (employee.getType().equals("Сдельная")) {
+            tax = 0.15;
+        }
+        double taxAmount = employee.getSalary() * tax;
+        double amountToPay = employee.getSalary() - taxAmount;
+        System.out.println(employee.getName() + "\t" + (tax * 100) + "\t\t" + employee.getSalary() + "\t\t" + amountToPay);
+    }
+}
